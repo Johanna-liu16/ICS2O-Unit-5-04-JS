2486435c53fc6b9648f9480a13eaf8467a0eebf5
Johanna Liu <99354992+Johanna-liu16@users.noreply.github.com>
2022-05-04 01:26:05 +0000

    added

--- js/script.js
+++ js/script.js
@@ -4,29 +4,28 @@
 // Created on: Mar 2022
 // This file contains the JS functions for index.html
 
+;("use strict")
+
 /**
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS2O-Unit-5-02-HTML/sw.js", {
+    scope: "/ICS2O-Unit-5-02-HTML/",
   })
 }
 
-;("use strict")
 /**
- * This function calculates area of a parrallelogram.
+ * This function compares slider with random number.
  */
-function calculate() {
-  // input
-  const length = parseInt(document.getElementById("length-of-pyramid").value)
-  const width = parseInt(document.getElementById("width-of-pyramid").value)
-  const height = parseInt(document.getElementById("height-of-pyramid").value)
+function myButtonClicked() {
+  var day = document.getElementById("day").value
+  var age = document.getElementById("age").value
 
   // process
-  const volume = (length * width * height) / 3
-
-  // output
-  document.getElementById("answers").innerHTML =
-    "Volume is: " + volume.toFixed(2) + " cm³"
+  if (( day == "tuesday" || day == "thursday") || (age > 12 && age < 21)) {
+      document.getElementById("answer").innerHTML= "You're eligible for student pricing"
+  } else {
+      document.getElementById("answer").innerHTML= "You must pay regular price"
+  }
 }
